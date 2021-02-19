daf94b797ef0601893097c9fb62389e125d2981f
Nkemjika Okeke <nkemjikaokeke@Erics-MacBook-Air-3191.local>
2021-02-18 21:58:08 -0500

    update classifiers

--- NOkeke/predictive_classifiers.py
+++ NOkeke/predictive_classifiers.py
@@ -18,6 +18,9 @@ target_test = pd.read_csv('Data/target_test.csv', index_col=0)
 target_train = label_binarize(target_train.values, classes=[0, 1, 2])
 target_test = label_binarize(target_test.values, classes=[0, 1, 2])
 
+# Make a list converting class number to class name, for plotting
+class_to_num = {0: '1/N', 1: 'RP', 2: 'MVO'}
+
 '''
 Logistic Regression Classifier
 '''
@@ -29,14 +32,11 @@ logreg_score = logreg.decision_function(features_test)
 # Initialize dictionaries
 precision = {}
 recall = {}
-# Make a list converting class number to class name, for plotting
-class_to_num = {0: '1/N', 1: 'RP', 2: 'MVO'}
 
 # Plot the PR curve for each class
 for i in range(3):
     precision[i], recall[i], _ = precision_recall_curve(target_test[:, i],
                                                         logreg_score[:, i])
-
     plt.figure()
     plt.step(recall[i], precision[i], where='post')
 
@@ -45,4 +45,56 @@ for i in range(3):
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
     plt.title('Logistic Regression Precision-Recall Curve; Class: '+class_to_num[i])
+    plt.show()
+
+'''
+MLP Classifier
+'''
+# Fit classifier
+mlp = OneVsRestClassifier(MLPClassifier(max_iter=1500, random_state=0))
+mlp.fit(features_train.values, target_train)
+mlp_score = mlp.predict_proba(features_test)
+
+# Initialize dictionaries
+precision = {}
+recall = {}
+
+# Plot the PR curve for each class
+for i in range(3):
+    precision[i], recall[i], _ = precision_recall_curve(target_test[:, i],
+                                                        mlp_score[:, i])
+    plt.figure()
+    plt.step(recall[i], precision[i], where='post')
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('MLP Precision-Recall Curve; Class: '+class_to_num[i])
+    plt.show()
+
+'''
+Random Forest Classifier
+'''
+# Fit classifier
+rf = OneVsRestClassifier(RandomForestClassifier())
+rf.fit(features_train.values, target_train)
+rf_score = rf.predict_proba(features_test)
+
+# Initialize dictionaries
+precision = {}
+recall = {}
+
+# Plot the PR curve for each class
+for i in range(3):
+    precision[i], recall[i], _ = precision_recall_curve(target_test[:, i],
+                                                        rf_score[:, i])
+    plt.figure()
+    plt.step(recall[i], precision[i], where='post')
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Random Forest Precision-Recall Curve; Class: '+class_to_num[i])
     plt.show()
